f3e6dcf81fee4464d6598be0ec1541c7ccd19f26
Jean Trujillo <00zacks00@gmail.com>
2024-05-03 12:00:21 -0500

    funcion de editar varios funcional

--- modelos/modelo_productos.py
+++ modelos/modelo_productos.py
@@ -3,27 +3,22 @@ from librerias import *
 class Formulario():
 
     def get_productos(self):
-        # Conexión a la instancia de MongoDB en localhost
         client = MongoClient('mongodb+srv://jeantpdev:2HH3bRjoUMrUMGYU@productos.ns6gatt.mongodb.net/')
         db = client.Productos
         
-        # Recuperar todos los documentos de la colección 'products'
         lista_productos = db.lista_productos.find()
-        
-        # Crear una lista para almacenar los datos recuperados
         datos = []
-        
-        # Iterar sobre los documentos y agregarlos a la lista
+
         for producto in lista_productos:
             datos.append(producto)
         
-        # Imprimir o devolver los datos como JSON
         return jsonify({"productos": datos})
     
     def insertar_producto(self):
-        # Conexión a la instancia de MongoDB en localhost
+
         client = MongoClient('mongodb+srv://jeantpdev:2HH3bRjoUMrUMGYU@productos.ns6gatt.mongodb.net/')
         db = client.Productos
+
         id = request.json['_id']
         nombre = request.json['nombre_producto']
         categoria = request.json['categoria']
@@ -35,7 +30,6 @@ class Formulario():
         dimensiones = request.json['dimensiones']
         material = request.json['material']
 
-        # Crear el documento a insertar en la colección
         documento = {
             "_id": id,
             "nombre_producto": nombre,
@@ -49,50 +43,41 @@ class Formulario():
             "material": material
         }
         
-        # Insertar el documento en la colección
         db.lista_productos.insert_one(documento)
         
         return jsonify({"mensaje": "Producto insertado correctamente"})
     
     def editar_campo_producto(self, id_producto):
-        # Conexión a la instancia de MongoDB en localhost
+
         client = MongoClient('mongodb+srv://jeantpdev:2HH3bRjoUMrUMGYU@productos.ns6gatt.mongodb.net/')
         db = client.Productos
         id = str(id_producto)
-        imagenes_productos = request.json['imagenes_productos']
+        
+        nombre_producto = request.json['nombre_producto']
+        categoria = request.json['categoria']
+        precio = request.json['precio']
+        descripcion = request.json['descripcion']
 
-        print(id)
-        print(imagenes_productos)
+        info_producto_editar = {
+            "nombre_producto": nombre_producto,
+            "categoria": categoria,
+            "descripcion": descripcion,
+            "precio": precio
+        }
 
-        # Actualizar el campo imagen_principal del documento con el _id especificado
-        db.lista_productos.update_one(
+        db.lista_productos.update_many(
             {"_id": id},
-            {"$set": {"imagenes_productos": imagenes_productos}}
+            {"$set": info_producto_editar}
         )
 
-        return jsonify({"mensaje": "Imagenes de productos actualizada correctamente"})
-    
-    def editar_campo_producto(self, id_producto):
-        # Conexión a la instancia de MongoDB en localhost
-        client = MongoClient('mongodb+srv://jeantpdev:2HH3bRjoUMrUMGYU@productos.ns6gatt.mongodb.net/')
-        db = client.Productos
-        id = str(id_producto)
-        imagenes_productos = request.json['imagenes_productos']
-
-        # Actualizar el campo imagen_principal del documento con el _id especificado
-        db.lista_productos.update_one(
-            {"_id": id},
-            {"$set": {"imagenes_productos": imagenes_productos}}
-        )
-
-        return jsonify({"mensaje": "Imagenes de productos actualizada correctamente"})
+        return jsonify({"mensaje": "Producto actualizado correctamente"})
     
     def eliminar_producto(self, id_producto):
-        # Conexión a la instancia de MongoDB en localhost
         client = MongoClient('mongodb+srv://jeantpdev:2HH3bRjoUMrUMGYU@productos.ns6gatt.mongodb.net/')
         db = client.Productos
+        
         id = str(id_producto)
-        # Eliminar el documento con el _id especificado
+
         resultado = db.lista_productos.delete_one({"_id": id})
 
         # Verificar si se eliminó el documento
